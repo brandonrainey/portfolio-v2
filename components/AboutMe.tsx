e593c41fe7d169d82bd9455fb0a929b9a317abeb
Brandon Rainey <brandonhrainey@gmail.com>
2023-08-16 20:53:32 -0400

    fixed escaped characters

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -20,12 +20,12 @@ export default function AboutMe({ AboutMeRef }: AboutMeProps) {
       <p className="border-l-4 pl-2 border-[#37448f]">
         As a 29-year-old Frontend Developer based in Goose Creek, South
         Carolina, I am passionate about leveraging technology to create
-        impactful solutions. For the past two years, I've been honing my skills
+        impactful solutions. For the past two years, I&apos;ve been honing my skills
         in HTML, CSS, and JavaScript/TypeScript, with a focus on developing
         engaging websites and web applications using JavaScript and React.
       </p>
       <p className="border-l-4 pl-2 border-[#37448f]">
-        A key project I'm particularly proud of is a responsive e-commerce site
+        A key project I&apos;m particularly proud of is a responsive e-commerce site
         I developed, featuring Stripe integration for seamless transactions and
         secure user authentication via Google OAuth. I utilized technologies
         such as Next.js, TypeScript, Redux Toolkit, and Tailwind CSS to create a
@@ -35,8 +35,8 @@ export default function AboutMe({ AboutMeRef }: AboutMeProps) {
         Short-term, I aspire to transition from a non-technical role into a web
         development position. Long-term, I aim to continually expand my
         knowledge, with the goal of becoming a senior developer. As a
-        self-taught programmer, I'm a testament to self-motivation and have a
-        love for learning new things - currently, I'm learning Japanese.
+        self-taught programmer, I&apos;m a testament to self-motivation and have a
+        love for learning new things - currently, I&apos;m learning Japanese.
       </p>
       <p className="border-l-4 pl-2 border-[#37448f]">
         Aside from technical skills, I bring adaptability, effective teamwork,
@@ -49,7 +49,7 @@ export default function AboutMe({ AboutMeRef }: AboutMeProps) {
         latest tech trends such as AI, PC gaming, and watching anime. Currently,
         I am actively seeking opportunities as a Frontend Developer and would
         love to bring my skills, dedication, and enthusiasm to your team. If
-        you're looking for a passionate and ambitious developer, please don't
+        you&apos;re looking for a passionate and ambitious developer, please don't
         hesitate to reach out!
       </p>
     </div>
