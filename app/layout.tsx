5c4fd261acf168b727a394c2fd7f598f55838926
Brandon Rainey <brandonhrainey@gmail.com>
2024-01-08 18:18:12 -0500

    changed metadata description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import './globals.css'
+import type { Metadata } from 'next'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Frontend Developer Portfolio',
-  description: 'Developer portfolio of Brandon Rainey',
+  description: "Greetings! I'm Brandon Rainey, a frontend developer passionate about crafting interactive and visually appealing websites. Explore my portfolio featuring projects that showcase my skills in technologies like Typescript and React. Eager to contribute fresh perspectives and technical acumen to innovative teams. Let's connect and discuss how I can add value to your next web development endeavor.",
 }
 
 export default function RootLayout({
