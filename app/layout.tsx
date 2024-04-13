5861a0fdc84b69693286fef7535afe75cf0e4732
Brandon Rainey <brandonhrainey@gmail.com>
2024-04-12 23:40:20 -0400

    metadata changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Arimo } from 'next/font/google'
 export const metadata: Metadata = {
   title: 'Frontend Developer Portfolio',
   description:
-    "I'm Brandon Rainey, a frontend developer passionate about crafting interactive and visually appealing websites. Explore my portfolio featuring projects that showcase my skills in technologies like Typescript and React. Eager to contribute fresh perspectives and technical acumen to innovative teams. Let's connect and discuss how I can add value to your next web development endeavor.",
+    "I'm Brandon Rainey, a Frontend Developer passionate about crafting interactive and visually appealing websites. Explore my portfolio featuring projects that showcase my skills in technologies like Typescript and React. Eager to contribute fresh perspectives and technical acumen to innovative teams. Let's connect and discuss how I can add value to your next web development endeavor.",
 }
 
 const arimo = Arimo({
