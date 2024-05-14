f2caef408ebe622c696c90bf0ec8bdb9a5015755
Brandon Rainey <brandonhrainey@gmail.com>
2024-05-13 23:27:05 -0400

    changes to about me

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -40,7 +40,7 @@ export default function AboutMe({ AboutMeRef }: AboutMeProps) {
       </p>
       <p className="border-l-4 pl-2 border-[#37448f]">
         Outside of professional interests, I enjoy staying up-to-date with the
-        latest tech trends, PC gaming, and watching anime. 
+        latest tech trends like AI, PC gaming, and watching anime. 
         I am currently seeking opportunities as a Frontend Developer and would
         love to bring my skills, dedication, and enthusiasm to your team. If
         you&apos;re looking for a passionate and ambitious developer, please
