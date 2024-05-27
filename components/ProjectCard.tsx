d448f066bcfcbac211428a4e0c5179247c92bdfa
Brandon Rainey <brandonhrainey@gmail.com>
2024-05-26 22:39:55 -0400

    added key to card skills map

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -28,11 +28,11 @@ export default function ProjectCard({ project, skills }: ProjectCardProps) {
       </div>
       <div className="flex mt-auto">
         <div className="flex items-center">
-          {project.skills.map((skill: string) =>
+          {project.skills.map((skill: string, index) =>
             skills.map(
               (s) =>
                 s.name === skill && (
-                  <div className={`sm:h-14 w-10 sm:w-14 h-10 p-1 flex items-center justify-center rounded-full border-2 border-white/50 bg-[#444059] -ml-2 shadow shadow-slate-700`}>
+                  <div className={`sm:h-14 w-10 sm:w-14 h-10 p-1 flex items-center justify-center rounded-full border-2 border-white/50 bg-[#444059] -ml-2 shadow shadow-slate-700`} key={index}>
                     <img
                       src={s.icon}
                       className={`  object-contain object-center w-full h-full rounded-full `}
