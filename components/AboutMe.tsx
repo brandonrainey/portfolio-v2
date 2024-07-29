244c7df360460a065c97df4979063d552a87a34a
Brandon Rainey <brandonhrainey@gmail.com>
2024-07-28 23:50:51 -0400

    changes to about me text and small position change on desktop

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -15,35 +15,33 @@ export default function AboutMe({ AboutMeRef }: AboutMeProps) {
         <h2 className="text-5xl text-[#9bc1e7] font-bold">About Me</h2>
       </div>
 
-      <div className="grid md:grid-cols-2 grid-cols-1 gap-4">
+      <div className="grid md:grid-cols-2 grid-cols-1 gap-4 sm:gap-y-12">
         <p className="border rounded-lg p-2 border-[#37448f7a] z-20 relative aboutPanel shadow-sm shadow-cyan-400">
-          Hi, I&apos;m a Frontend Developer currently based in Goose Creek, SC,
-          and I&apos;m passionate about leveraging technology to create
-          impactful solutions. For over the past several years, I&apos;ve been
-          honing my skills in technologies like HTML, CSS, and JavaScript/TypeScript, with a focus
-          on developing websites using JavaScript/React.
+          Hi, I'm a Frontend Developer based in Goose Creek, SC, passionate
+          about using technology to create impactful solutions. Over the past
+          few years, I've been honing my skills in HTML, CSS, and
+          JavaScript/TypeScript, with a focus on building websites using React.
         </p>
 
         <p className="border rounded-lg p-2 border-[#37448f7a] z-20 relative aboutPanel shadow-sm shadow-cyan-400">
-          A key project of mine is an events platform site I developed,
-          featuring Stripe integration for transactions, user
-          authentication via Clerk, and MongoDB as a database. I utilized technologies such as
-          Next.js, TypeScript, and Tailwind CSS to create a
-          modern interface and user experience.
+          One of my key projects is an events platform site that I developed,
+          featuring Stripe integration for transactions, user authentication via
+          Clerk, and MongoDB as the database. I used Next.js, TypeScript, and
+          Tailwind CSS to create a modern interface and user experience.
         </p>
+
         <p className="border rounded-lg p-2 border-[#37448f7a] z-20 relative aboutPanel shadow-sm shadow-cyan-400">
-          Long-term, I aim to continually expand my
-          knowledge, with the goal of becoming a senior-level developer and beyond. As
-          a self-taught programmer, I&apos;m a testament to self-motivation and
-          have a love for learning new things - currently, I&apos;m also
-          learning Japanese.
+          My long-term goal is to continually expand my knowledge and eventually
+          become a senior-level developer. As a self-taught programmer, I'm
+          driven by self-motivation and a love for learning new things.
+          Currently, I'm also learning Japanese.
         </p>
+
         <p className="border rounded-lg p-2 border-[#37448f7a] z-20 relative aboutPanel shadow-sm shadow-cyan-400">
-           I&apos;m
-          currently seeking opportunities as a Frontend Developer and would love
-          to bring my skills, dedication, and enthusiasm to your team. If
-          you&apos;re looking for a passionate and ambitious developer, please
-          don&apos;t hesitate to reach out!
+          I'm seeking opportunities as a Frontend Developer and would love to
+          bring my skills, dedication, and enthusiasm to your team. If you're
+          looking for a passionate and ambitious developer, please feel free to
+          reach out!
         </p>
       </div>
     </section>
